890702b65f849f25979b7e4a9efebd97dd2bfdf9
Sachin Bishnoi 12220000 <119493865+sachinbishnoi12220000@users.noreply.github.com>
2022-12-22 15:31:02 +0530

    Add files via upload

---
+++ Airthmatic operator11.py
@@ -0,0 +1,9 @@
+num1 = float(input(" enter first number :"))# taking input for first number
+num2 = float(inpur("enter second number :"))# taking input for second number
+#apply the logic for airthmatic operators
+sumResult = num1 + num2
+print ("sum of two numbers is : ", sumresult )
+print ("sum of ", num1, "and ", num2,"is:", sumresult )
+subResult = num1-num2
+print("subtraction of two is :",subresult )
+mulresult = num1*num2
