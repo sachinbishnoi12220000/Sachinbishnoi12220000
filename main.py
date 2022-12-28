a40fe2d58b640947df7dbcc9c05db64645b80a30
Sachin Bishnoi 12220000 <119493865+sachinbishnoi12220000@users.noreply.github.com>
2022-12-28 15:26:27 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+def add(x,y):
+    print("sum of two numbers  is :", x+y)
+
+def sub(x,y):
+    print("subtraction of two numbers is :",x-y)
+def mul(a,b):
+    print("multiplication of two number is :",x*y)
+
+a = int(input("Enter first number :"))
+b = int(input("Enter second number :"))
+add(a,b) # function call wioth arguments
+sub(a,b)
+mul(a,b)
